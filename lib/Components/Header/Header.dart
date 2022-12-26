1247cd57baa9bc3251608fe0ff74b223c4e80e39
Sanjana60 <78793118+Sanjana60@users.noreply.github.com>
2022-12-27 00:15:23 +0530

    commit

--- lib/Components/Header/Header.dart
+++ lib/Components/Header/Header.dart
@@ -11,11 +11,11 @@ class _HeaderState extends State<Header> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(vertical: 70,horizontal: 20),
+      padding: EdgeInsets.symmetric(vertical: 70, horizontal: 20),
       child: Stack(
         children: [
           Positioned(
-            left: MediaQuery.of(context).size.width/3.5,
+            left: MediaQuery.of(context).size.width / 3.5,
             top: 50,
             child: Row(
               children: const [
@@ -45,10 +45,79 @@ class _HeaderState extends State<Header> {
             ),
           ),
           Positioned(
-            left: MediaQuery.of(context).size.width/4.3,
+            left: MediaQuery.of(context).size.width / 4.3,
             top: 85,
             child: Image(
               image: AssetImage('images/Vector.png'),
+            ),
+          ),
+          Positioned(
+            left: MediaQuery.of(context).size.width / 8.4,
+            top: 70,
+            child: Image(
+              image: AssetImage('images/Gradient.png'),
+            ),
+          ),
+          Positioned(
+            left: MediaQuery.of(context).size.width / 5.4,
+            top: 140,
+            child: Image(
+              image: AssetImage('images/image 1.png'),
+            ),
+          ),
+          Positioned(
+            left: MediaQuery.of(context).size.width / 3.5,
+            top: 140,
+            child: Container(
+              child: Column(
+                children: const [
+                  Text(
+                    'A Designer who',
+                    style: TextStyle(
+                      color: Color(0xFFFFFFFF),
+                      letterSpacing: 0.5,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 17.0,
+                      fontFamily: 'Preahvihear',
+                      decoration: TextDecoration.underline,
+                      fontStyle: FontStyle.normal,
+                    ),
+                  ),
+                  Text(
+                    'Judges a book by its',
+                    style: TextStyle(
+                      color: Color(0xFFFFFFFF),
+                      letterSpacing: 0.5,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 23.0,
+                      fontFamily: 'Preahvihear',
+                      fontStyle: FontStyle.normal,
+                    ),
+                  ),
+                  Text(
+                    'cover',
+                    style: TextStyle(
+                      color: Color(0xFF7127BA),
+                      letterSpacing: 0.5,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 23.0,
+                      fontFamily: 'Preahvihear',
+                      fontStyle: FontStyle.normal,
+                    ),
+                  ),
+                  Text(
+                    '...',
+                    style: TextStyle(
+                      color: Color(0xFFFFFFFF),
+                      letterSpacing: 0.5,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 23.0,
+                      fontFamily: 'Preahvihear',
+                      fontStyle: FontStyle.normal,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
